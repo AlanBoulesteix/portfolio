a77755ec25f5a3c971c91764a9724750abe18451
AlanBoulesteix <alan.boulesteix2312@gmail.com>
2024-12-13 17:35:52 +0100

    add smooth scroll

--- src/component/Navbar/Navbar.tsx
+++ src/component/Navbar/Navbar.tsx
@@ -2,22 +2,33 @@ import './Navbar.styles.css';
 import { Profile } from '../Presentation/Profile';
 
 function Navbar() {
+  const handleScroll = (event: React.SyntheticEvent) => {
+    event.preventDefault();
+    const target = event.target as HTMLAnchorElement;
+    const targetId = target.getAttribute('href')?.replace('#', '');
+    const targetElement = document.getElementById(targetId || '');
+
+    if (targetElement) {
+      targetElement.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
   return (
     <>
-        <Profile />
-        <nav>
-          <ul>
-            <li>
-              <a href="#about">About</a>
-            </li>
-            <li>
-              <a href="#experiences">Experiences</a>
-            </li>
-            <li>
-              <a href="#schoolProject">School Project</a>
-            </li>
-          </ul>
-        </nav>
+      <Profile />
+      <nav>
+        <ul onClick={handleScroll}>
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#experiences">Experiences</a>
+          </li>
+          <li>
+            <a href="#schoolProject">School Project</a>
+          </li>
+        </ul>
+      </nav>
     </>
   );
 }
