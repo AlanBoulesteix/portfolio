2697203b2ed464170eb4faedbce8dc8c00219b09
AlanBoulesteix <alan.boulesteix2312@gmail.com>
2024-12-14 21:07:50 +0100

    Refacto About

--- src/component/About/About.tsx
+++ src/component/About/About.tsx
@@ -1,13 +1,13 @@
+import { ABOUTPARAGRAPHS } from '../../constant/about';
+import './About.styles.css';
+
 function About() {
   return (
     <>
       <div id="about" className="section">
-        <a>
-          After 3 years in retails and 2 years working for banking company, I
-          decided to go back to my first intuiton: Coding. For doing so, I
-          enrolled into 42 school in Paris. Witch was a revelation. Since then I
-          had the opportunite to learn a lot with all the school projets.
-        </a>
+      {ABOUTPARAGRAPHS.map((paragraph, index) => (
+        <p key={index}>{paragraph}</p>
+      ))}
       </div>
     </>
   );
