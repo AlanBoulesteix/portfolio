043dfaa18b9a9009f637785ff8793ba9c63987fc
AlanBoulesteix <alan.boulesteix2312@gmail.com>
2024-12-15 18:06:41 +0100

    add active into navLink

--- src/component/Navbar/Navbar.tsx
+++ src/component/Navbar/Navbar.tsx
@@ -1,7 +1,14 @@
 import './Navbar.styles.css';
 import { Profile } from '../Presentation/Profile';
+import React from 'react';
 
 function Navbar() {
+  const [isActive, setIsActive] = React.useState({
+    about: false,
+    experiences: false,
+    schoolProject: false,
+  });
+
   const handleScroll = (event: React.SyntheticEvent) => {
     event.preventDefault();
     const target = event.target as HTMLAnchorElement;
@@ -13,19 +20,45 @@ function Navbar() {
     }
   };
 
+  const handleActive = (anchor: string) => {
+    setIsActive({
+      about: anchor === 'about',
+      experiences: anchor === 'experiences',
+      schoolProject: anchor === 'schoolProject',
+    });
+  };
+
   return (
     <>
       <Profile />
       <nav>
         <ul onClick={handleScroll}>
           <li>
-            <a href="#about">About</a>
+            <a
+              href="#about"
+              className={isActive.about ? 'active' : ''}
+              onClick={() => handleActive('about')}
+            >
+              About
+            </a>
           </li>
           <li>
-            <a href="#experiences">Experiences</a>
+            <a
+              href="#experiences"
+              className={isActive.experiences ? 'active' : ''}
+              onClick={() => handleActive('experiences')}
+            >
+              Experiences
+            </a>
           </li>
           <li>
-            <a href="#schoolProject">School Project</a>
+            <a
+              href="#schoolProject"
+              className={isActive.schoolProject ? 'active' : ''}
+              onClick={() => handleActive('schoolProject')}
+            >
+              School Project
+            </a>
           </li>
         </ul>
       </nav>
