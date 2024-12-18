e45a55976d23a7f054358bd7e9a598de696c7d6f
AlanBoulesteix <alan.boulesteix2312@gmail.com>
2024-12-18 15:32:07 +0100

    small fix

--- src/component/home/Home.tsx
+++ src/component/home/Home.tsx
@@ -1,27 +1,21 @@
-import { useState } from 'react';
 import About from '../About/About';
 import ExperiencesPage from '../ExperiencesPage/ExperiencesPage';
 import Navbar from '../Navbar/Navbar';
 import { Profile } from '../Presentation/Profile';
 import './Home.styles.css';
-import useIntersectionObserver from '../../hook/useIntersectionObserver';
-import SchoolProject from '../SchoolProject/SchoolProject';
 
 export default function Home() {
-  const [activeSection, setActiveSection] = useState('about');
-
-  useIntersectionObserver(setActiveSection);
   return (
     <>
       <div className="homeDiv">
         <div className="navbar">
           <Profile />
-          <Navbar activeSection={activeSection} />
+          <Navbar />
         </div>
         <div className="content">
           <About />
           <ExperiencesPage />
-          <SchoolProject />
+          {/* <SchoolProject /> */}
         </div>
       </div>
     </>
