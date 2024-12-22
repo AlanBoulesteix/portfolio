21119c2aed7249b2a03d77181a22627bcdb55d68
AlanBoulesteix <alan.boulesteix@gmail.com>
2024-12-22 18:40:51 +0100

    Try to fix path for vercel

--- src/constant/contact.ts
+++ src/constant/contact.ts
@@ -1,5 +1,5 @@
 export enum ContactLink {
   github = 'https://github.com/AlanBoulesteix',
   linkedin = 'https://www.linkedin.com/in/alan-boulesteix-528755103/',
-  curriculumVitae = '../../public/CV.pdf',
+  curriculumVitae = 'CV.pdf',
 }
