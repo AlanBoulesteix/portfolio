e6e1e1eecd8af9615d3bb619d3b99cd6d4dbb3fd
AlanBoulesteix <alan.boulesteix@gmail.com>
2025-01-02 15:47:51 +0100

    fix spelling

--- src/constant/about.ts
+++ src/constant/about.ts
@@ -1,20 +1,17 @@
 export const ABOUT_PARAGRAPHS = [
-"After 3 years in retails and 2 years working for banking company, I \
-decided to go back to my first intuiton: Coding. For doing so, I \
-enrolled into 42 school in Paris. Witch was a revelation. Thanks to this school, I \
-had the opportunite to learn a lot. Multi-threading, algorithm complexity, pointers, linked list, binary tree, and so on. \
-I also had the chance to work on a lot of projects, from a shell to a full web site. \
-Thank's to this school, I worked with a lot of different people, from different background, different culture, different age. \
+"After 3 years in retail and 2 years working for a banking company, I decided to go back to my first intuition: Coding. \
+To do so, I enrolled into 42 school in Paris. Which was a revelation. Thanks to this school, I had the opportunity to learn a lot. \
+Multi-threading, algorithm complexity, pointers, linked list, binary tree, and so on. \
+I also had the chance to work on a lot of projects, from a shell to a full web site. Thanks to this school, I worked with a lot of different people, from different backgrounds, different cultures, different ages. \
 I learned a lot from them.",
 
-"Following a year and a half in 42, I had the opportunite to do an internship as Full Stack Developer at a company called Legal Place. \
-This mission gave me an idea of how thinks work. From the conception of the project to the deployment. \
-I was hire for create a proof of concept of a new API. I had to create a server that can use this API and see what we could do with it.\
-After this mission, I was integrated into the team and I had the chance to work on a lot of different jobs. \
-From fixing iFrame bug, using aws lambda ans Simple Queue Service, find a way to de-deuplicate multiple webhooks calls, and so on.",
+"Following a year and a half in 42, I had the opportunity to do an internship as Full Stack Developer at a company called Legal Place. \
+This mission gave me an idea of how things work. From the conception of the project to the deployment. I was hired to create a proof of concept of a new API. \
+I had to create a server that can use this API and see what we could do with it. After this mission, I was integrated into the team and I had the chance to work on a lot of different jobs. \
+From fixing iFrame bugs, using aws lambda and Simple Queue Service, finding a way to de-duplicate multiple webhooks calls, and so on",
 
-"After this internship, Legal Place wanted to hire me, but I had a new challenge in mind. I wanted to go work a cross the ocean... \
-So I left France and I came to Canada. Me, my motivation, my smile and my visa, are looking for an opportunite to grow and learn more.",
+"After this internship, Legal Place wanted to hire me, but I had a new challenge in mind. I wanted to go work across the ocean... \
+So I left France and I came to Canada. Me, my motivation, my smile and my visa, are looking for an opportunity to grow and learn more.",
 
-"In my free time, I like doing all kind of sport, from gym to climbing through running. I also like to read and video games."
+"In my free time, I like doing all kinds of sports, from gym to climbing through running. I also like to read and play video games."
 ];
