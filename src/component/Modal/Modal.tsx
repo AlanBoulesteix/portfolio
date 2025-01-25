c5ede6914b511258fd623dcfa65a92b34858812d
AlanBoulesteix <alan.boulesteix@gmail.com>
2025-01-25 16:13:39 -0500

    add modal

---
+++ src/component/Modal/Modal.tsx
@@ -0,0 +1,42 @@
+import ReactDOM from 'react-dom';
+import './Modal.styles.css';
+import { useEffect } from 'react';
+
+type ModalProps = {
+  isOpen: boolean;
+  onClose: () => void;
+  children: React.ReactNode;
+};
+
+export default function Modal({ isOpen, onClose, children }: ModalProps) {
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (event.key === 'Escape') {
+        onClose();
+      }
+    };
+
+    if (isOpen) {
+      document.addEventListener('keydown', handleKeyDown);
+    } else {
+      document.removeEventListener('keydown', handleKeyDown);
+    }
+
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
+    };
+  });
+
+  if (!isOpen) return null;
+
+  const divToFocus = document.getElementById('modal-root');
+
+  return ReactDOM.createPortal(
+    <div className="modal-backdrop" onClick={onClose}>
+      <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+        {children}
+      </div>
+    </div>,
+    divToFocus ?? document.body,
+  );
+}
---
+++ src/component/Modal/ModalBalise.tsx
@@ -0,0 +1,3 @@
+export default function ModalBalise() {
+  return <div id="modal-root"></div>;
+}
