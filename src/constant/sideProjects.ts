a7631634cabbd9ae4f88f24c96dd23358d123974
AlanBoulesteix <alan.boulesteix@gmail.com>
2025-02-02 23:02:27 -0500

    rename files

---
+++ src/constant/sideProjects.ts
@@ -0,0 +1,14 @@
+const sideProjects = [
+  {
+    title: 'NFT Card',
+    description: 'A simple NFT card that can be used for various purposes.',
+    screenshot: '/screenshots/nft-card.png',
+    url: 'https://nft-preview-card-component-boulesteixs-projects.vercel.app/',
+  },
+  {
+    title: 'Advice Generator App',
+    description: 'An app that generates random advice using slip advice Api.',
+    screenshot: '/screenshots/advice-generator.png',
+    url: 'https://advice-generator-app-boulesteixs-projects.vercel.app/',
+  }
+]
