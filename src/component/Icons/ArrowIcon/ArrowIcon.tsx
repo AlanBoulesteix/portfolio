e413f84dbdff1e872c0d5ff031792deeef94966b
AlanBoulesteix <alan.boulesteix@gmail.com>
2025-02-03 18:37:54 -0500

    update Arrow

--- src/component/Icons/ArrowIcon/ArrowIcon.tsx
+++ src/component/Icons/ArrowIcon/ArrowIcon.tsx
@@ -10,12 +10,18 @@ export default function ArrowIcon({ color }: ArrowIconProps) {
     <div className="arrow-icon">
       <svg
         xmlns="http://www.w3.org/2000/svg"
-        width="20"
-        height="20"
         viewBox="0 0 20 20"
         fill={iconColor}
+        aria-hidden="true"
+        width="20"
+        height="20"
+
       >
-        <path d="M0,12v6a2,2,0,0,0,2,2H18a2,2,0,0,0,2-2V2a2,2,0,0,0-2-2H2A2,2,0,0,0,0,2V8H8V5l5,5L8,15V12Z" />
+        <path
+          fillRule="evenodd"
+          d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z"
+          clipRule="evenodd"
+        ></path>
       </svg>
     </div>
   );
