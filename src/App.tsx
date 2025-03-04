a6e6148250b51ebe2b0e3db592b8402c54df4566
AlanBoulesteix <alan.boulesteix@gmail.com>
2025-03-04 11:33:44 -0500

    add speedInsights

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import './App.css';
 import Home from './component/Home/Home';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 export default function App() {
   return (
     <>
-        <BrowserRouter>
-          <Routes>
-            <Route index element={<Home />} />
-          </Routes>
-        </BrowserRouter>
+      <SpeedInsights />
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
